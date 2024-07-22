f96eea6f102352e307c9e02a03608449c1c385c6
Maestro1982 <kris.tote@telenet.be>
2024-07-22 14:43:38 +0200

    create signUp and login server actions and zod validation schema's that export also a type

---
+++ src/app/(auth)/sign-up/actions.ts
@@ -0,0 +1,87 @@
+"use server";
+
+import { hash } from "@node-rs/argon2";
+import { generateIdFromEntropySize } from "lucia";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { isRedirectError } from "next/dist/client/components/redirect";
+
+import { signUpSchema, SignUpValues } from "@/lib/validation";
+import prisma from "@/lib/prisma";
+
+import { lucia } from "@/auth";
+
+export async function signUp(
+  credentials: SignUpValues,
+): Promise<{ error: string }> {
+  try {
+    const { username, email, password } = signUpSchema.parse(credentials);
+
+    const passwordHash = await hash(password, {
+      memoryCost: 19456,
+      timeCost: 2,
+      outputLen: 32,
+      parallelism: 1,
+    });
+
+    const userId = generateIdFromEntropySize(10);
+
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        username: {
+          equals: username,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (existingUser) {
+      return {
+        error: "Username already in use.",
+      };
+    }
+
+    const existingEmailAddress = await prisma.user.findFirst({
+      where: {
+        email: {
+          equals: email,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    if (existingEmailAddress) {
+      return {
+        error: "Email address already in use.",
+      };
+    }
+
+    // Create a new user in the database
+    await prisma.user.create({
+      data: {
+        id: userId,
+        username,
+        displayName: username,
+        email,
+        passwordHash,
+      },
+    });
+
+    // Immediately setup a session so we don't need to login after signing up
+    const session = await lucia.createSession(userId, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+    cookies().set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes,
+    );
+
+    return redirect("/");
+  } catch (error) {
+    if (isRedirectError(error)) throw error;
+    console.error(error);
+    return {
+      error: "Something went wrong. Please try again.",
+    };
+  }
+}
---
+++ src/lib/validation.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+const requiredString = z.string().trim().min(1, "Required");
+
+export const signUpSchema = z.object({
+  email: requiredString.email("Invalid email address"),
+  username: requiredString.regex(
+    /^[a-zA-Z0-9_-]+$/,
+    "Only letters, numbers, - and _ are allowed",
+  ),
+  password: requiredString.regex(
+    /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/,
+    "Password must contain one digit from 1 to 9, one lowercase letter, one uppercase letter, one special character, no space, and it must be 8-16 characters long.",
+  ),
+});
+
+export type SignUpValues = z.infer<typeof signUpSchema>;
+
+export const loginSchema = z.object({
+  username: requiredString,
+  password: requiredString,
+});
+
+export type LoginValues = z.infer<typeof loginSchema>;
